0cb37adbe7b869005184d6d5c4a94af1a95312ea
Koltsov Artur <artur-koltsov@list.ru>
2025-11-14 18:03:37 +0300

    Init project

---
+++ src/test/java/org/example/avito_testtask_1/AvitoTestTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.avito_testtask_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvitoTestTask1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
